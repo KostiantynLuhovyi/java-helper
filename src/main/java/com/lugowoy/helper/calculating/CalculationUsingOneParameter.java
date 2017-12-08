e10338da1d6401f573d953de47452f227c59d2e1
LugowoyKonstantin <kostya.lugowoy@gmail.com>
2017-12-08 22:43:12 +0200

    08.12.2017  22:38  • Write and edit documentation classes in the package com.lugowoy.helper.calculating .  • Edit file README.md .

---
+++ src/main/java/com/lugowoy/helper/calculating/CalculationUsingOneParameter.java
@@ -0,0 +1,29 @@
+package com.lugowoy.helper.calculating;
+
+/**
+ * Created by Konstantin Lugowoy on 03-Feb-17.
+ *
+ * @author Konstantin Lugowoy
+ * @version 1.1
+ * @since 03.02.2017
+ * <p>
+ * A functional interface that declares a contract to implement a function,
+ *                                            and also provides a function for computing using a one input parameter.
+ * </p>
+ * @param <T> - The type of data that will be returned by the result of the function.
+ * @param <K> - The type of object that passed to parameter of the function.
+ */
+
+@FunctionalInterface
+public interface CalculationUsingOneParameter<T, K> {
+
+    /**
+     * <p>
+     * A function that performs calculations using a single input parameter.
+     * </p>
+     * @param kVar - The object passed by the parameter to the function and whose data will be used for the calculations.
+     * @return The result of the execution (calculation) of the function is returned.
+     */
+    T calculate(K kVar);
+
+}
