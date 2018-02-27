7742a36ef2346758f0655f357f25f40a1a6be798
LugowoyKonstantin <kostya.lugowoy@gmail.com>
2018-02-27 21:51:16 +0200

    27.02.2018   21:50  • Added a create (T coordinateX, T coordinateY) method to the FactoryPoint factory    class to create a Point class object.

--- src/main/java/com/lugowoy/helper/factory/FactoryPoint.java
+++ src/main/java/com/lugowoy/helper/factory/FactoryPoint.java
@@ -7,7 +7,7 @@ import com.lugowoy.helper.models.points.Point;
  * Created by Konstantin Lugowoy on 08-Jan-18.
  *
  * @author Konstantin Lugowoy
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  *
  * <p>
@@ -22,6 +22,20 @@ public class FactoryPoint<T extends Number> extends Factory<Point<T>> {
 
     private FactoryPoint(CreatorPoint<T> creatorPoint) {
         super(creatorPoint);
+    }
+
+    /**
+     * <p>
+     * The method executes the creation of an object of {@link Point} class by calling the constructor of parameters.
+     * </p>
+     *
+     * @param coordinateX The value of the coordinate X.
+     * @param coordinateY The value of the coordinate Y.
+     * @return Created object.
+     * @since 1.1
+     */
+    public Point<T> create(T coordinateX, T coordinateY) {
+        return ((CreatorPoint<T>)super.getCreating()).create(coordinateX, coordinateY);
     }
 
     /**
