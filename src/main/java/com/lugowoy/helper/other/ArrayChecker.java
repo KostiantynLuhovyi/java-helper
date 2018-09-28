8e28c1c9343a9c352b67118ba0b64bcae6834d1e
LugowoyKonstantin <kostya.lugowoy@gmail.com>
2018-09-28 17:14:32 +0300

    28.09.2018  17:11  • Removed throws of exceptions from methods for checking arrays in the ArrayChecker class from a package com.lugowoy.helper.other ;

--- src/main/java/com/lugowoy/helper/other/ArrayChecker.java
+++ src/main/java/com/lugowoy/helper/other/ArrayChecker.java
@@ -4,7 +4,7 @@ import com.lugowoy.helper.models.arrays.Array;
 
 /**
  * @author Konstantin Lugowoy
- * @version 1.0
+ * @version 1.1
  * @since 1.2
  *
  * An interface that performs static functions to perform checks on the correctness of various values associated with arrays.
@@ -14,60 +14,54 @@ public interface ArrayChecker {
 
     /**
      * Returns the result of the check object of the Array class passed by argument to is a null.
-     *
      * @param array The object of the Array class to check.
      * @return Result of the check.
      */
-    static <T> boolean checkArrayNonNull(Array<T> array) throws IllegalArgumentException {
+    static <T> boolean checkArrayNonNull(Array<T> array) {
+        boolean result = false;
         if (array != null) {
-            return true;
-        } else {
-            throw new IllegalArgumentException(
-                    new NullPointerException("The object of the Array class passed by the argument is null."));
+            result = true;
         }
+        return result;
     }
 
     /**
-     * Returns the result of the check array passed by argument to is a null.
-     *
+     * Returns the result of the check array passed by argument to is a null.     *
      * @param array The array to check.
      * @return Result to check.
      */
-    static <T> boolean checkArrayNonNull(T[] array) throws IllegalArgumentException {
+    static <T> boolean checkArrayNonNull(T[] array) {
+        boolean result = false;
         if (array != null) {
-            return true;
-        } else {
-            throw new IllegalArgumentException(
-                    new NullPointerException("The array passed by the argument is null."));
+            result = true;
         }
+        return result;
     }
 
     /**
      * Returns the result of the check value of the length of the array passed by argument is equal to or greater than zero.
-     *
      * @param lengthOfArray The value of the length of the array.
      * @return Result to check.
      */
-    static boolean checkLengthOfArrayIsEqualToOrGreaterThanZero(int lengthOfArray) throws IllegalArgumentException {
+    static boolean checkLengthOfArrayIsEqualToOrGreaterThanZero(int lengthOfArray) {
+        boolean result = false;
         if (lengthOfArray >= 0) {
-            return true;
-        } else {
-            throw new IllegalArgumentException("The value of the length of the array passed by the argument is equal to or greater than zero.");
+            result = true;
         }
+        return result;
     }
 
     /**
      * Returns the result of the check value of the length of the array passed by argument is greater than zero.
-     *
      * @param lengthOfArray The value of the length of the array.
      * @return Result to check.
      */
-    static boolean checkLengthOfArrayIsGreaterZero(int lengthOfArray) throws IllegalArgumentException {
+    static boolean checkLengthOfArrayIsGreaterZero(int lengthOfArray) {
+        boolean result = false;
         if (lengthOfArray > 0) {
-            return true;
-        } else {
-            throw new IllegalArgumentException("The value of the length of the array passed by the argument is greater than zero.");
+            result = true;
         }
+        return result;
     }
 
 }
