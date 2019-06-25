565d7565b72148f68140b2a2785f19ff7b96209e
LugowoyKonstantin <kostya.lugowoy@gmail.com>
2019-06-25 19:11:16 +0300

    25.06.2019  19:08  • Add class com.lugowoy.helper.other.MatrixAttributes for work (definition) with such matrix attributes as its rows and columns. Having created this class, the exception of repeated code repetition in solving various problems has been achieved.  • Changed (new) version of the project (see pom.xml file). From 1.7.1 to 1.7.2;

---
+++ src/main/java/com/lugowoy/helper/other/MatrixAttributes.java
@@ -0,0 +1,69 @@
+package com.lugowoy.helper.other;
+
+import com.lugowoy.helper.io.reading.Reading;
+
+import java.util.Objects;
+
+/**
+ * Created by LugowoyKonstantin on 25.06.2019.
+ * @version 1.0
+ * @since 1.7.2
+ */
+// add doc's
+public class MatrixAttributes {
+
+    private int rows = 1;
+    private int columns = 1;
+
+    private MatrixAttributes() {
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    static MatrixAttributes getInstanceMatrixAttributes(Reading reader) {
+        if (Objects.nonNull(reader)) {
+            MatrixAttributes matrixAttributes = new MatrixAttributes();
+            matrixAttributes.setRows(readMatrixRow(reader));
+            matrixAttributes.setColumns(readMatrixColumn(reader));
+            return matrixAttributes;
+        } else {
+            throw new NullPointerException("The reader is equal null.");
+        }
+    }
+
+    static MatrixAttributes getInstanceMatrixAttributes(int rows, int columns) {
+        if ((rows > 0 && rows < Integer.MAX_VALUE) && (columns > 0 && columns < Integer.MAX_VALUE)) {
+            MatrixAttributes matrixAttributes = new MatrixAttributes();
+            matrixAttributes.setRows(rows);
+            matrixAttributes.setColumns(columns);
+            return matrixAttributes;
+        } else {
+            throw new IllegalArgumentException("The arguments rows or columns is incorrect.");
+        }
+    }
+
+    private static int readMatrixRow(Reading reader) {
+        System.out.println("Enter the number of rows for the matrix : ");
+        return reader.readInt();
+    }
+
+    private static int readMatrixColumn(Reading reader) {
+        System.out.println("Enter the number of columns for the matrix : ");
+        return reader.readInt();
+    }
+
+}
