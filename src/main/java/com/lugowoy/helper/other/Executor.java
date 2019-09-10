e824278a2dec148e40be587be2bdd021bb158e64
lugowoy-konstantin <kostya.lugowoy@gmail.com>
2019-09-10 22:38:48 +0300

    10.09.2019   22:38  • Deleted com.lugowoy.helper.other.Runtime class.  • Written full implementation class com.lugowoy.helper.other.Executor.

--- src/main/java/com/lugowoy/helper/other/Executor.java
+++ src/main/java/com/lugowoy/helper/other/Executor.java
@@ -1,34 +1,61 @@
 package com.lugowoy.helper.other;
 
+import java.io.OutputStream;
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by LugowoyKonstantin on 01.09.2019.
  *
  * @author LugowoyKonstantin
- * @version 1.0
+ * @version 1.1
  * @since 1.7.4
  */
 //todo add doc's (class, methods)
-public class Executor<T> {
+//todo refactoring this class
+public class Executor {
 
-    T execute(Callable<T> callable) {
-        //todo the functionality of initializing the start and end time of a task should be taken out into another method
+    public static <T> T execute(Callable<T> callable) {
         long startExecuteTime, endExecuteTime;
         startExecuteTime = System.currentTimeMillis();
-
         T result = null;
         try {
             result = callable.call();
         } catch (Exception e) {
+            //todo to refactoring
             e.printStackTrace();
         }
-
         endExecuteTime = System.currentTimeMillis();
-        System.out.println("Task time in milliseconds : " + (endExecuteTime - startExecuteTime));
-
+        long runtime = (endExecuteTime - startExecuteTime);
+        System.out.printf("Runtime : %d,%d .", convertMillisecondsToMinutes(runtime), convertMillisecondsToSeconds(runtime));
         //todo check non null result
         return result;
+    }
+
+    public static <T> T execute(Callable<T> callable, OutputStream outputStreamMsg, String resultMsg) {
+        long startExecuteTime, endExecuteTime;
+        startExecuteTime = System.currentTimeMillis();
+        T result = null;
+        try {
+            result = callable.call();
+            outputStreamMsg.write(resultMsg.getBytes());
+        } catch (Exception e) {
+            //todo to refactoring
+            e.printStackTrace();
+        }
+        endExecuteTime = System.currentTimeMillis();
+        long runtime = (endExecuteTime - startExecuteTime);
+        System.out.printf("Runtime : %d,%d .", convertMillisecondsToMinutes(runtime), convertMillisecondsToSeconds(runtime));
+        //todo check non null result
+        return result;
+    }
+
+    private static long convertMillisecondsToMinutes(long milliseconds) {
+        return TimeUnit.MILLISECONDS.toMinutes(milliseconds);
+    }
+
+    private static long convertMillisecondsToSeconds(long milliseconds) {
+        return TimeUnit.MILLISECONDS.toSeconds(milliseconds);
     }
 
 }
