1f0cdf10f47a4b9ce3c4e0a68408035f2a16b886
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-02-06 15:18:30 +0200

    06.02.2020  15:18  • Test second master-dev branch.

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -6,6 +6,7 @@ public class Test {
     public static void main(String[] args) throws Exception {
 
         System.out.println("Test master-dev branch.");
+        System.out.println("Test second master-dev branch.");
 
     }
 
