789013fe880edd11359fe897add03b7dc01f1be5
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-02-15 23:59:45 +0200

    15.02.2020  23:59  • Full refactoring and write doc's of the code in the class  CheckerIndex .

--- src/main/java/com/lugowoy/helper/utils/checking/CheckerIndex.java
+++ src/main/java/com/lugowoy/helper/utils/checking/CheckerIndex.java
@@ -1,54 +1,55 @@
 package com.lugowoy.helper.utils.checking;
 
 /**
+ * This class contains static functionality for checking the index value for correctness.
  * <p> Created by Konstantin Lugowoy on 08.06.2019
  *
  * @author Konstantin Lugowoy
- * @version 1.2
+ * @version 1.3
  * @since 1.6.6
  */
-//todo write doc's
 public final class CheckerIndex {
 
-    public static final String MSG_INDEX_IS_OUT_OF_RANGE = "Index is out of range.";
+    private static final String MSG_EXCEPTION_INDEX_IS_OUT_OF_RANGE = "Index is out of range.";
+
     private static final int LOWER_INDEX = 0;
 
     private CheckerIndex() {
     }
 
-    public static boolean checkIndex(final int index, final int upperBoundIndex) {
-        boolean resultCheckIndex = false;
-        if (CheckerBound.isCorrectBound(upperBoundIndex, LOWER_INDEX, Integer.MAX_VALUE)) {
-            if (index >= LOWER_INDEX && index < upperBoundIndex) {
-                resultCheckIndex = true;
-            } else {
-                throw new IndexOutOfBoundsException(MSG_INDEX_IS_OUT_OF_RANGE);
-            }
-        }
-        return resultCheckIndex;
-    }
-
-    public static void checkIndexTest(final int index, final int upperBoundIndex) {
-        if (CheckerBound.isCorrectBound(upperBoundIndex, LOWER_INDEX, Integer.MAX_VALUE)) {
-            if (index < LOWER_INDEX || index > upperBoundIndex) {
-                throw new IndexOutOfBoundsException(MSG_INDEX_IS_OUT_OF_RANGE);
-            }
+    /**
+     * Check the argument {@code index} for correct entry into a range of values
+     * from {@link CheckerIndex#LOWER_INDEX} argument to {@code upperBoundIndex}
+     *
+     * @param index           Index value to check.
+     * @param upperBoundIndex Upper bound value to check index.
+     * @throws IndexOutOfBoundsException If the value {@code index} is not in the range of valid values
+     *                                   from {@link CheckerIndex#LOWER_INDEX} to {@code upperBoundIndex}.
+     */
+    public static void checkIndex(int index, int upperBoundIndex) {
+        CheckerBound.isCorrectBound(upperBoundIndex, LOWER_INDEX, Integer.MAX_VALUE);
+        if (index < LOWER_INDEX || index > upperBoundIndex) {
+            throw new IndexOutOfBoundsException(MSG_EXCEPTION_INDEX_IS_OUT_OF_RANGE);
         }
     }
 
-    public static boolean checkIndex(final int index, final int lowerBoundIndex, final int upperBoundIndex) {
-        boolean resultCheckIndex = false;
-        if (CheckerBound.isCorrectBound(lowerBoundIndex, LOWER_INDEX, Integer.MAX_VALUE)
-                && CheckerBound.isCorrectBound(upperBoundIndex, LOWER_INDEX, Integer.MAX_VALUE)) {
-            if (CheckerBound.isLowerBoundLessOrEqualThanUpperBound(lowerBoundIndex, upperBoundIndex)) {
-                if (index >= lowerBoundIndex && index <= upperBoundIndex) {
-                    resultCheckIndex = true;
-                } else {
-                    throw new IndexOutOfBoundsException(MSG_INDEX_IS_OUT_OF_RANGE);
-                }
-            }
+    /**
+     * Check the argument {@code index} for correct entry into a range of values
+     * from {@code lowerBoundIndex} argument to {@code upperBoundIndex}
+     *
+     * @param index           Index value to check.
+     * @param lowerBoundIndex Lower bound value to check index.
+     * @param upperBoundIndex Upper bound value to check index.
+     * @throws IndexOutOfBoundsException If the value {@code index} is not in the range of valid values
+     *                                   from {@code lowerBoundIndex} to {@code upperBoundIndex}.
+     */
+    public static void checkIndex(int index, int lowerBoundIndex, int upperBoundIndex) {
+        CheckerBound.isCorrectBound(lowerBoundIndex, LOWER_INDEX, Integer.MAX_VALUE);
+        CheckerBound.isCorrectBound(upperBoundIndex, LOWER_INDEX, Integer.MAX_VALUE);
+        CheckerBound.isLowerBoundLessOrEqualThanUpperBound(lowerBoundIndex, upperBoundIndex);
+        if (index < lowerBoundIndex && index > upperBoundIndex) {
+            throw new IndexOutOfBoundsException(MSG_EXCEPTION_INDEX_IS_OUT_OF_RANGE);
         }
-        return resultCheckIndex;
     }
 
 }
