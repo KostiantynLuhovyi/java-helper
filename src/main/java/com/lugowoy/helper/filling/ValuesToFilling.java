0ed0d46a905c5b5c41b9eec8bc8d0726de08145c
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-03-15 23:54:15 +0200

    docs(filling): Changed documentation ValueToFilling interface

--- src/main/java/com/lugowoy/helper/filling/ValuesToFilling.java
+++ src/main/java/com/lugowoy/helper/filling/ValuesToFilling.java
@@ -1,53 +1,54 @@
 package com.lugowoy.helper.filling;
 
 /**
- * The interface provides a set of constants that are used as default values to filling objects or storage(data structures).
+ * The interface contains a set of constants that used as default values
+ * for filling objects, storages (data structures), etc.
+ *
  * <p>Created by Konstantin Lugowoy on 08-Jan-18.
  *
  * @author Konstantin Lugowoy
- * @version 1.6
+ * @version 1.7
  * @since 1.7.8
  */
-//TODO edit doc's
 public interface ValuesToFilling {
 
     /**
-     * The integer value by default.
+     * The integer default value.
      */
     int DEFAULT_INT_VALUE = 0;
 
     /**
-     * The double value by default.
+     * The double default value.
      */
     double DEFAULT_DOUBLE_VALUE = 0.0;
 
     /**
-     * The integer zero value of the bound.
+     * The integer zero value.
      */
     int INT_ZERO = 0;
 
     /**
-     * The double zero value of the bound.
+     * The double zero value.
      */
     double DOUBLE_ZERO = 0d;
 
     /**
-     * The integer value of the lower (negative) boundary by default.
+     * The integer default value of the lower (negative) boundary.
      */
     int INT_LOWER_BOUND = -10;
 
     /**
-     * The integer value of the upper (positive) boundary by default.
+     * The integer default value of the upper (positive) boundary.
      */
     int INT_UPPER_BOUND = 10;
 
     /**
-     * The double value of the lower (negative) boundary by default.
+     * The double default value of the lower (negative) boundary.
      */
     double DOUBLE_LOWER_BOUND = -10.0;
 
     /**
-     * The double value of the upper (positive) boundary by default.
+     * The double default value of the upper (positive) boundary.
      */
     double DOUBLE_UPPER_BOUND = 10.0;
 
