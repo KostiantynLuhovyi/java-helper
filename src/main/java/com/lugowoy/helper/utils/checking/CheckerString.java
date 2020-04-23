14b844549d5ff3321e1c7d591ef0f972fee5e1b3
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-04-23 22:49:36 +0300

    docs(utils) Fixes documentation in the class ../utils.checking.CheckerString.java

--- src/main/java/com/lugowoy/helper/utils/checking/CheckerString.java
+++ src/main/java/com/lugowoy/helper/utils/checking/CheckerString.java
@@ -17,12 +17,13 @@ public class CheckerString {
     private static final int DEFAULT_LOWER_STRING = 0;
 
     /**
-     * Checks the {@code lengthString} value in the valid range from {@link Long#MIN_VALUE} to {@link Long#MAX_VALUE}.
+     * Checks the {@code lengthString} value in the valid range
+     * from {@value DEFAULT_LOWER_STRING} to {@link Integer#MAX_VALUE}.
      *
-     * @param lengthString the string length value to check.
+     * @param lengthString the value length of the string to check.
      *
      * @throws LengthStringOutOfRangeException if the {@code lengthString} value out of range
-     * from {@link Long#MIN_VALUE} to {@link Long#MAX_VALUE}.
+     * from {@value DEFAULT_LOWER_STRING} to {@link Integer#MAX_VALUE}.
      */
     public static void checkLengthString(int lengthString) {
         checkLengthString(lengthString, DEFAULT_LOWER_STRING, Integer.MAX_VALUE);
@@ -32,9 +33,12 @@ public class CheckerString {
      * Checks the {@code lengthString} value in the valid range from {@code lowerBoundLength} to {@code upperBoundLength}.
      *
      * @param lengthString the string length value to check.
-     * @param lowerBoundLength the string length lower bound to check.
-     * @param upperBoundLength the string length upper bound to check.
+     * @param lowerBoundLength the lower bound length of the string to check.
+     * @param upperBoundLength the upper bound length of the string to check.
      *
+     * @throws BoundOutOfRangeException if the any of boundary arguments out of range
+     * from {@link Integer#MIN_VALUE} to {@link Integer#MAX_VALUE}.
+     * @throws BoundsCompareException if the {@code lowerBoundLength} greater or equal than {@code upperBoundLength}.
      * @throws LengthStringOutOfRangeException if the {@code lengthString} value out of range
      * from {@code lowerBoundLength} to {@code upperBoundLength}.
      */
