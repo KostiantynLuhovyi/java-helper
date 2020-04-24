81272a63d590153551d13ade78a899be0ebcfc20
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-04-24 14:46:03 +0300

    docs(utils) Editing documentation in the class ../utils.CreatorString.java

--- src/main/java/com/lugowoy/helper/utils/CreatorString.java
+++ src/main/java/com/lugowoy/helper/utils/CreatorString.java
@@ -16,21 +16,23 @@ import java.util.Objects;
 public final class CreatorString {
 
     /**
-     * Constructs an object of this class
+     * Constructs an object of this class.
      */
     public CreatorString() {
     }
 
     /**
-     * Creates a string of length as a {@code lengthString} argument from the pseudo-random characters
-     * contained in the {@code characters} array.
+     * Creates a string of length as a {@code lengthString} argument
+     * from the pseudo-random characters contained in the {@code characters} array.
      *
      * @param characters the array of characters from which the string creates.
      * @param lengthString the length of created string.
      *
+     * @return the created string.
+     *
      * @throws NullPointerException if the {@code characters} array is null.
-     * @throws LengthStringOutOfRangeException if the {@code lengthString} value out of range
-     * from 0 to {@link Integer#MAX_VALUE}.
+     * @throws LengthStringOutOfRangeException if the {@code lengthString} value
+     * out of range from 0 to {@link Integer#MAX_VALUE}.
      * @throws LengthStringOutOfRangeException if the {@code lengthString} value out of correct range.
      * @see Objects#requireNonNull(Object)
      * @see CheckerString#checkLengthString(int)
@@ -48,19 +50,23 @@ public final class CreatorString {
     }
 
     /**
-     * Creates a string with a length as a pseudo-random number ranging from {@code lowerBoundLength}
-     * to {@code upperBoundLength} and containing pseudo-random characters contained in the {@code characters} array.
+     * Creates a string with a length as a pseudo-random number ranging
+     * from {@code lowerBoundLength} to {@code upperBoundLength}
+     * and containing pseudo-random characters contained in the {@code characters} array.
      *
      * @param characters the array of characters from which the string creates.
      * @param lowerBoundLength the lower boundary length value of string.
      * @param upperBoundLength the upper boundary length value of string.
      *
-     * @throws BoundOutOfRangeException if the any of boundary arguments out of range
-     * from 0 to {@link Integer#MAX_VALUE}.
-     * @throws BoundsCompareException if the {@code lowerBoundLength} greater or equal than {@code upperBoundLength}.
+     * @return the created string
+     *
+     * @throws BoundOutOfRangeException if the any of boundary arguments
+     * out of range from 0 to {@link Integer#MAX_VALUE}.
+     * @throws BoundsCompareException if the {@code lowerBoundLength} greater
+     * or equal than {@code upperBoundLength}.
      * @throws NullPointerException if the {@code characters} array is null.
-     * @throws LengthStringOutOfRangeException if the {@code lengthString} value out of range
-     * from 0 to {@link Integer#MAX_VALUE}.
+     * @throws LengthStringOutOfRangeException if the {@code lengthString} value
+     * out of range from 0 to {@link Integer#MAX_VALUE}.
      * @throws LengthStringOutOfRangeException if the {@code lengthString} value out of correct range.
      * @see CheckerBound#isCorrectBound(Number, Number)
      * @see CheckerBound#isLowerBoundLessUpperBound(Number, Number)
