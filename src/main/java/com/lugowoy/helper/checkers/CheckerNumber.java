ab470112c5a08780bdbaf53029ec86a28a0fd9e8
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-02 22:48:23 +0300

    refactor(./utils.checking) File CheckerNumber.java moved to com.lugowoy.helper.checkers package

--- src/main/java/com/lugowoy/helper/checkers/CheckerNumber.java
+++ src/main/java/com/lugowoy/helper/checkers/CheckerNumber.java
@@ -1,4 +1,4 @@
-package com.lugowoy.helper.utils.checking;
+package com.lugowoy.helper.checkers;
 
 import com.lugowoy.helper.checkers.BoundsComparisonException;
 import com.lugowoy.helper.checkers.CheckerBound;
