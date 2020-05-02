7cd4d937b184722341d01725cf7e4d498b29753a
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-02 22:20:22 +0300

    refactor(./utils.checking) Changes to the BoundOutOfRangeException class
    The class now inherits from the class com.lugowoy.helper.OutOfRangeException

--- src/main/java/com/lugowoy/helper/utils/BoundOutOfRangeException.java
+++ src/main/java/com/lugowoy/helper/utils/BoundOutOfRangeException.java
@@ -1,14 +1,16 @@
 package com.lugowoy.helper.utils;
 
+import com.lugowoy.helper.OutOfRangeException;
+
 /**
  * Thrown when various boundary value is not in the valid range of values.
  * <p> Created by Konstantin Lugowoy on 06.09.2019.
  *
  * @author Konstantin Lugowoy
- * @version 1.1
+ * @version 1.2
  * @since 1.7.5
  */
-public class BoundOutOfRangeException extends RuntimeException {
+public class BoundOutOfRangeException extends OutOfRangeException {
 
     /**
      * Constructs a new runtime exception with the specified detail message.
