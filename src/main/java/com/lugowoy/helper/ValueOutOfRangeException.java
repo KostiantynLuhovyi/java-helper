d7d075c05944e9b9ebb591027eba75db4b4d6a8a
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-03 21:53:59 +0300

    feature(.) Change in class ValueOutOfRangeException
    In class ValueOutOfRangeException parent class is changed to OutOfRangeException.
    Added constructors inherited from the parent class.

--- src/main/java/com/lugowoy/helper/ValueOutOfRangeException.java
+++ src/main/java/com/lugowoy/helper/ValueOutOfRangeException.java
@@ -2,24 +2,82 @@ package com.lugowoy.helper;
 
 /**
  * Thrown when the value is out of range.
- * <p> Created by Konstantin Lugowoy on 27.09.2019.
+ * <p>
+ * Created by Konstantin Lugowoy on 27.09.2019.
  *
  * @author Konstantin Lugowoy
- * @version 1.2
+ * @version 1.3
  * @since 1.8
  */
-public class ValueOutOfRangeException extends RuntimeException {
+public class ValueOutOfRangeException extends OutOfRangeException {
 
     /**
-     * Constructs a new runtime exception with the specified detail message.
-     * The cause is not initialized, and may subsequently be initialized by a
-     * call to {@link #initCause}.
+     * Constructs a new runtime exception with the specified detail message. The
+     * cause is not initialized, and may subsequently be initialized by a call
+     * to {@link #initCause}.
      *
-     * @param message the detail message. The detail message is saved for
-     *                later retrieval by the {@link #getMessage()} method.
+     * @param message the detail message. The detail message is saved for later
+     * retrieval by the {@link #getMessage()} method.
      */
     public ValueOutOfRangeException(String message) {
         super(message);
+    }
+
+    /**
+     * Constructs a new runtime exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with {@code cause} is
+     * <i>not</i> automatically incorporated in this runtime exception's detail
+     * message.
+     *
+     * @param message the detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
+     * @param cause the cause (which is saved for later retrieval by the {@link
+     * #getCause()} method).  (A {@code null} value is permitted, and indicates
+     * that the cause is nonexistent or unknown.)
+     *
+     * @since 1.4
+     */
+    public ValueOutOfRangeException(final String message,
+                                    final Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new runtime exception with the specified cause and a detail
+     * message of {@code (cause==null ? null : cause.toString())} (which
+     * typically contains the class and detail message of {@code cause}).  This
+     * constructor is useful for runtime exceptions that are little more than
+     * wrappers for other throwables.
+     *
+     * @param cause the cause (which is saved for later retrieval by the {@link
+     * #getCause()} method).  (A {@code null} value is permitted, and indicates
+     * that the cause is nonexistent or unknown.)
+     *
+     * @since 1.4
+     */
+    public ValueOutOfRangeException(final Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Constructs a new runtime exception with the specified detail message,
+     * cause, suppression enabled or disabled, and writable stack trace enabled
+     * or disabled.
+     *
+     * @param message the detail message.
+     * @param cause the cause.  (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     * @param enableSuppression whether or not suppression is enabled or
+     * disabled
+     * @param writableStackTrace whether or not the stack trace should be
+     * writable
+     *
+     * @since 1.7
+     */
+    public ValueOutOfRangeException(final String message, final Throwable cause,
+                                    final boolean enableSuppression,
+                                    final boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 
 }
