5825912c62e7b6bb572f2543126e65af5f9a29a8
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-05 21:34:23 +0300

    refactor(.) Formatting BoundOutOfRangeException class code

--- src/main/java/com/lugowoy/helper/BoundOutOfRangeException.java
+++ src/main/java/com/lugowoy/helper/BoundOutOfRangeException.java
@@ -1,8 +1,9 @@
 package com.lugowoy.helper;
 
 /**
- * Thrown when various boundary value is not in the valid range of values.
- * <p> Created by Konstantin Lugowoy on 06.09.2019.
+ * Thrown when various boundary value is out of range of values.
+ * <p>
+ * Created by Konstantin Lugowoy on 06.09.2019.
  *
  * @author Konstantin Lugowoy
  * @version 1.3
@@ -11,14 +12,14 @@ package com.lugowoy.helper;
 public class BoundOutOfRangeException extends OutOfRangeException {
 
     /**
-     * Constructs a new runtime exception with the specified detail message.
-     * The cause is not initialized, and may subsequently be initialized by a
-     * call to {@link #initCause}.
+     * Constructs a new runtime exception with the specified detail message. The
+     * cause is not initialized, and may subsequently be initialized by a call
+     * to {@link #initCause}.
      *
-     * @param message the detail message. The detail message is saved for
-     *                later retrieval by the {@link #getMessage()} method.
+     * @param message the detail message. The detail message is saved for later
+     * retrieval by the {@link #getMessage()} method.
      */
-    public BoundOutOfRangeException(String message) {
+    public BoundOutOfRangeException(final String message) {
         super(message);
     }
 
