414f5f90ce2789e600daca140a4c96e6d8fa5948
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-05 17:49:46 +0300

    refactor(./utils) Files (classes) moved to com.lugowoy.helper.models.matrices package
    Moved files :
    MatrixRowOutOfRangeException.java
    MatrixColumnOutOfRangeException.java
    In this package, their purpose will be better displayed.

--- src/main/java/com/lugowoy/helper/models/matrices/MatrixColumnOutOfRangeException.java
+++ src/main/java/com/lugowoy/helper/models/matrices/MatrixColumnOutOfRangeException.java
@@ -1,14 +1,16 @@
-package com.lugowoy.helper.utils;
+package com.lugowoy.helper.models.matrices;
+
+import com.lugowoy.helper.OutOfRangeException;
 
 /**
- * Thrown when the value of the matrix row is out of range.
+ * Thrown when the value of the matrix column is out of range.
  * <p> Created by Konstantin Lugowoy on 10.06.2019.
  *
  * @author Konstantin Lugowoy
  * @version 1.1
  * @since 1.7
  */
-public class MatrixRowOutOfRangeException extends RuntimeException {
+public class MatrixColumnOutOfRangeException extends RuntimeException {
 
     /**
      * Constructs a new runtime exception with the specified detail message.
@@ -18,7 +20,7 @@ public class MatrixRowOutOfRangeException extends RuntimeException {
      * @param message the detail message. The detail message is saved for
      *                later retrieval by the {@link #getMessage()} method.
      */
-    public MatrixRowOutOfRangeException(String message) {
+    public MatrixColumnOutOfRangeException(String message) {
         super(message);
     }
 
