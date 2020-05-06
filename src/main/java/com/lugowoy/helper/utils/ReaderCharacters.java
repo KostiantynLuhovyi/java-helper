06ddf84ab70e9f140fc3ea00cbbbd1d2488545d2
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-06 20:41:26 +0300

    refactor(utils) Refactoring code in the ReaderCharacters class

--- src/main/java/com/lugowoy/helper/utils/ReaderCharacters.java
+++ src/main/java/com/lugowoy/helper/utils/ReaderCharacters.java
@@ -6,17 +6,19 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 /**
  * The class provides functionality for reading characters from specific files.
- * <p> Created by Konstantin Lugowoy on 10.06.2018.
+ * <p>
+ * Created by Konstantin Lugowoy on 10.06.2018.
  *
  * @author Konstantin Lugowoy
- * @version 1.5
+ * @version 1.6
  * @since 1.2
  */
 public final class ReaderCharacters {
@@ -28,40 +30,50 @@ public final class ReaderCharacters {
     }
 
     /**
-     * Reads characters from all files found at addresses in the {@code pathsToFiles} argument.
-     * The read characters write to an array of type char and returned by the result of the method.
+     * Reads characters from all files found at addresses in the {@code
+     * pathsToFiles} argument and write to an array of type char.
      *
-     * @param pathToFiles paths to files storing characters.
+     * @param pathsToFiles paths to files storing characters.
      *
      * @return char array filled with read characters.
      *
-     * @throws NullPointerException if the {@code pathsToFiles} argument is null.
-     * @throws NullPointerException if objects for reading characters, for some reason is null.
+     * @throws NullPointerException if the {@code pathsToFiles} null.
+     * @throws NullPointerException if objects for reading characters, for some
+     * reason null.
      * @throws IOException if an I/O error occurs.
-     * @see Objects#requireNonNull(Object)
      */
-    public char[] readCharacters(final String... pathToFiles) throws IOException {
-        Objects.requireNonNull(pathToFiles);
+    public char[] readCharacters(final String... pathsToFiles)
+            throws IOException {
+        Objects.requireNonNull(pathsToFiles,
+                               "Array with paths to files is null.");
         List<Character> charactersList = new ArrayList<>();
         FileInputStream fileStream = null;
         InputStreamReader streamReader = null;
         BufferedReader reader = null;
-        try {
-            for (String fileName : pathToFiles) {
+        for (String fileName : pathsToFiles) {
+            Objects.requireNonNull(fileName, "File path is null.");
+            try {
                 fileStream = new FileInputStream(fileName);
-                streamReader = new InputStreamReader(fileStream, StandardCharsets.UTF_8);
+                streamReader = new InputStreamReader(fileStream, UTF_8);
                 reader = new BufferedReader(streamReader);
                 int character;
                 while ((character = reader.read()) != -1) {
                     charactersList.add((char) character);
                 }
+            } finally {
+                closeAllStreams(fileStream, streamReader, reader);
             }
-        } finally {
-            Objects.requireNonNull(fileStream).close();
-            Objects.requireNonNull(streamReader).close();
-            Objects.requireNonNull(reader).close();
         }
         return ArrayUtils.toPrimitive(charactersList.toArray(Character[]::new));
+    }
+
+    private void closeAllStreams(final FileInputStream fileStream,
+                                 final InputStreamReader streamReader,
+                                 final BufferedReader reader)
+            throws IOException {
+        Objects.requireNonNull(fileStream, "Stream is null.").close();
+        Objects.requireNonNull(streamReader, "Stream is null.").close();
+        Objects.requireNonNull(reader, "Reader is null.").close();
     }
 
 }
