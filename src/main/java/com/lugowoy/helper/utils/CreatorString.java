382228d7fa172af52aab516a6b12a4278caeb9b6
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-06 21:23:25 +0300

    refactor(utils) Refactoring code in the CreatorString class

--- src/main/java/com/lugowoy/helper/utils/CreatorString.java
+++ src/main/java/com/lugowoy/helper/utils/CreatorString.java
@@ -1,85 +1,87 @@
 package com.lugowoy.helper.utils;
 
-import com.lugowoy.helper.utils.checking.CheckerBound;
-import com.lugowoy.helper.utils.checking.CheckerString;
+import com.lugowoy.helper.checkers.CheckerBoundNumber;
+import com.lugowoy.helper.checkers.CheckerString;
 
 import java.util.Objects;
 
 /**
- * The class provides functionality for generating pseudorandom objects of the {@link String} class.
- * <p> Created by Konstantin Lugowoy on 21.04.2020.
+ * The class provides functionality for generating pseudorandom objects of the
+ * {@link String} class from character storage.
+ * <p>
+ * Created by Konstantin Lugowoy on 21.04.2020.
  *
  * @author Konstantin Lugowoy
- * @version 1.1
+ * @version 1.2
  * @since 3.0
  */
 public final class CreatorString {
+
+    private RandomNumber rand;
+    private StringBuffer strBuffer;
 
     /**
      * Constructs an object of this class.
      */
     public CreatorString() {
+        this.rand = new RandomNumber();
+        this.strBuffer = new StringBuffer();
     }
 
     /**
-     * Creates a string of length as a {@code lengthString} argument
-     * from the pseudo-random characters contained in the {@code characters} array.
+     * Creates a string of length as the value {@code lengthString} from the
+     * pseudo-random characters contained in the {@code characters} array. The
+     * maximum length of the created string is {@link Integer#MAX_VALUE}.
      *
      * @param characters the array of characters from which the string creates.
      * @param lengthString the length of created string.
      *
      * @return the created string.
      *
-     * @throws NullPointerException if the {@code characters} array is null.
-     * @throws LengthStringOutOfRangeException if the {@code lengthString} value
-     * out of range from 0 to {@link Integer#MAX_VALUE}.
-     * @throws LengthStringOutOfRangeException if the {@code lengthString} value out of correct range.
-     * @see Objects#requireNonNull(Object)
-     * @see CheckerString#checkLengthString(int)
-     * @see CheckerString#checkLengthString(int)
+     * @throws NullPointerException if the {@code characters} array is {@code
+     * null}.
+     * @throws LengthOutOfRangeException if the {@code lengthString} value out
+     * of range from {@literal 0} to {@link Integer#MAX_VALUE}.
      */
     public String create(final char[] characters, final int lengthString) {
-        CheckerString.checkLengthString(lengthString);
-        Objects.requireNonNull(characters);
-        StringBuilder stringBuilder = new StringBuilder();
-        RandomNumber random = new RandomNumber();
+        Objects.requireNonNull(characters, "Array of characters is null.");
+        CheckerString.checkLength(lengthString, 0, Integer.MAX_VALUE);
         for (int i = 0; i < lengthString; i++) {
-            stringBuilder.append(characters[random.generateInt(characters.length - 1)]);
+            this.strBuffer.append(this.rand.generateInt(characters.length - 1));
         }
-        return stringBuilder.toString();
+        String res = strBuffer.toString();
+        strBuffer.delete(0, strBuffer.length() - 1);
+        return res;
     }
 
     /**
-     * Creates a string with a length as a pseudo-random number ranging
-     * from {@code lowerBoundLength} to {@code upperBoundLength}
-     * and containing pseudo-random characters contained in the {@code characters} array.
+     * Creates a string with a length as a pseudo-random number ranging from
+     * {@code lowerLength} to {@code upperLength} and containing pseudo-random
+     * characters contained in the {@code characters} array. The maximum length
+     * of the created string is {@link Integer#MAX_VALUE}.
      *
      * @param characters the array of characters from which the string creates.
-     * @param lowerBoundLength the lower boundary length value of string.
-     * @param upperBoundLength the upper boundary length value of string.
+     * @param lowerLength the lower boundary length value of string.
+     * @param upperLength the upper boundary length value of string.
      *
-     * @return the created string
+     * @return the created string.
      *
-     * @throws BoundOutOfRangeException if the any of boundary arguments
-     * out of range from 0 to {@link Integer#MAX_VALUE}.
-     * @throws BoundsCompareException if the {@code lowerBoundLength} greater
-     * or equal than {@code upperBoundLength}.
      * @throws NullPointerException if the {@code characters} array is null.
-     * @throws LengthStringOutOfRangeException if the {@code lengthString} value
-     * out of range from 0 to {@link Integer#MAX_VALUE}.
-     * @throws LengthStringOutOfRangeException if the {@code lengthString} value out of correct range.
-     * @see CheckerBound#isCorrectBound(Number, Number)
-     * @see CheckerBound#isLowerBoundLessUpperBound(Number, Number)
-     * @see CreatorString#create(char[], int)
-     * @see CreatorString#create(char[], int)
-     * @see CheckerString#checkLengthString(int)
+     * @throws BoundOutOfRangeException if the any of boundary arguments out of
+     * range from {@literal 0} to {@link Integer#MAX_VALUE}.
+     * @throws BoundsComparisonException if the {@code lowerLength} greater or
+     * equal than {@code upperLength}.
+     * @throws LengthOutOfRangeException if the {@code lengthString} value out
+     * of range from {@literal 0} to {@link Integer#MAX_VALUE}.
      */
-    public String create(final char[] characters, final int lowerBoundLength, final int upperBoundLength) {
-        CheckerBound.isCorrectBound(lowerBoundLength, Integer.MAX_VALUE);
-        CheckerBound.isCorrectBound(upperBoundLength, Integer.MAX_VALUE);
-        CheckerBound.isLowerBoundLessUpperBound(lowerBoundLength, upperBoundLength);
-        int randomLengthString = new RandomNumber().generateInt(lowerBoundLength, upperBoundLength);
-        return this.create(characters, randomLengthString);
+    public String create(final char[] characters, final int lowerLength,
+                         final int upperLength) {
+        CheckerBoundNumber.checkInRange(lowerLength, 0, Integer.MAX_VALUE);
+        CheckerBoundNumber.checkInRange(upperLength, 0, Integer.MAX_VALUE);
+        CheckerBoundNumber.checkLowerLessUpper(lowerLength, upperLength);
+        int randLength = new RandomNumber().generateInt(lowerLength,
+                                                        upperLength);
+        return this.create(characters, randLength);
     }
 
 }
