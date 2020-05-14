8f554c26b743abb39a7217eca50de389afc6258b
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-14 17:04:09 +0300

    docs(utils) Formatting documentation in the BoundsComparisonException class

--- src/main/java/com/lugowoy/helper/utils/BoundsComparisonException.java
+++ src/main/java/com/lugowoy/helper/utils/BoundsComparisonException.java
@@ -14,8 +14,8 @@ public class BoundsComparisonException extends RuntimeException {
 
     /**
      * Constructs a new runtime exception with the specified detail message. The
-     * cause is not initialized, and may subsequently be initialized by a call to
-     * {@link #initCause}.
+     * cause is not initialized, and may subsequently be initialized by a call
+     * to {@link #initCause}.
      *
      * @param message the detail message. The detail message is saved for later
      * retrieval by the {@link #getMessage()} method.
@@ -30,11 +30,11 @@ public class BoundsComparisonException extends RuntimeException {
      * <i>not</i> automatically incorporated in this runtime exception's detail
      * message.
      *
-     * @param message the detail message (which is saved for later retrieval by the
-     * {@link #getMessage()} method).
+     * @param message the detail message (which is saved for later retrieval by
+     * the {@link #getMessage()} method).
      * @param cause the cause (which is saved for later retrieval by the {@link
-     * #getCause()} method).  (A {@code null} value is permitted, and indicates that
-     * the cause is nonexistent or unknown.)
+     * #getCause()} method).  (A {@code null} value is permitted, and indicates
+     * that the cause is nonexistent or unknown.)
      *
      * @since 1.4
      */
@@ -45,14 +45,14 @@ public class BoundsComparisonException extends RuntimeException {
 
     /**
      * Constructs a new runtime exception with the specified cause and a detail
-     * message of {@code (cause==null ? null : cause.toString())} (which typically
-     * contains the class and detail message of {@code cause}).  This constructor is
-     * useful for runtime exceptions that are little more than wrappers for other
-     * throwables.
+     * message of {@code (cause==null ? null : cause.toString())} (which
+     * typically contains the class and detail message of {@code cause}).  This
+     * constructor is useful for runtime exceptions that are little more than
+     * wrappers for other throwables.
      *
      * @param cause the cause (which is saved for later retrieval by the {@link
-     * #getCause()} method).  (A {@code null} value is permitted, and indicates that
-     * the cause is nonexistent or unknown.)
+     * #getCause()} method).  (A {@code null} value is permitted, and indicates
+     * that the cause is nonexistent or unknown.)
      *
      * @since 1.4
      */
@@ -61,15 +61,17 @@ public class BoundsComparisonException extends RuntimeException {
     }
 
     /**
-     * Constructs a new runtime exception with the specified detail message, cause,
-     * suppression enabled or disabled, and writable stack trace enabled or
-     * disabled.
+     * Constructs a new runtime exception with the specified detail message,
+     * cause, suppression enabled or disabled, and writable stack trace enabled
+     * or disabled.
      *
      * @param message the detail message.
-     * @param cause the cause.  (A {@code null} value is permitted, and indicates
-     * that the cause is nonexistent or unknown.)
-     * @param enableSuppression whether or not suppression is enabled or disabled
-     * @param writableStackTrace whether or not the stack trace should be writable
+     * @param cause the cause.  (A {@code null} value is permitted, and
+     * indicates that the cause is nonexistent or unknown.)
+     * @param enableSuppression whether or not suppression is enabled or
+     * disabled
+     * @param writableStackTrace whether or not the stack trace should be
+     * writable
      *
      * @since 1.7
      */
