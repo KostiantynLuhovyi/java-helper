e157320431c20d5202472d7483d2e40a8de20dd0
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-16 23:47:38 +0300

    docs(execution) Adding documentation in the ExecutionTime class

--- src/main/java/com/lugowoy/helper/execution/ExecutionTime.java
+++ src/main/java/com/lugowoy/helper/execution/ExecutionTime.java
@@ -6,6 +6,17 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 
 /**
+ * The class provides functionality for calculating the execution time of a
+ * task.<br> The start time of the task in milliseconds, and the end time are
+ * indicated, and the result is the value of the difference in milliseconds.
+ * When creating an object of this class, its attributes the start and
+ * end times are initialized with the value 0. To indicate the start of the run
+ * time, one of the setters must be called to set the start time.
+ * To end the run time, you also need to call the corresponding setter.
+ * To get the result, namely the difference between the end time
+ * and the start time, can be obtained by calling the {@link
+ * ExecutionTime#calculateExecutionTime()} method.
+ * <p>
  * Created by Konstantin Lugowoy on 13.09.2019.
  *
  * @author Konstantin Lugowoy
@@ -17,10 +28,23 @@ public class ExecutionTime {
     private long startMillis = 0L;
     private long endMillis = 0L;
 
+    /**
+     * Set the start time in milliseconds.<br> The start value in milliseconds
+     * is taken from the method {@link System#currentTimeMillis()} call.
+     */
     public void setStartTime() {
         this.startMillis = System.currentTimeMillis();
     }
 
+    /**
+     * Set the start time in milliseconds.<br> The start value in milliseconds
+     * is taken from the {@code milliseconds} argument.
+     *
+     * @param milliseconds the start time in milliseconds.
+     *
+     * @throws IllegalArgumentException if the {@code milliseconds} argument
+     * value is negative.
+     */
     public void setStartTime(final long milliseconds) {
         if (CheckerNumber.isPositive(milliseconds)
                             || CheckerNumber.isZero(milliseconds)) {
@@ -31,10 +55,23 @@ public class ExecutionTime {
         }
     }
 
+    /**
+     * Set the end time in milliseconds.<br> The end value in milliseconds is
+     * taken from the method {@link System#currentTimeMillis()} call.
+     */
     public void setEndTime() {
         this.endMillis = System.currentTimeMillis();
     }
 
+    /**
+     * Set the end time in milliseconds.<br> The end value in milliseconds is
+     * taken from the {@code milliseconds} argument.
+     *
+     * @param milliseconds the end time in milliseconds.
+     *
+     * @throws IllegalArgumentException if the {@code milliseconds} argument
+     * value is negative.
+     */
     public void setEndTime(final long milliseconds) {
         if (CheckerNumber.isPositive(milliseconds)
                             || CheckerNumber.isZero(milliseconds)) {
@@ -45,6 +82,10 @@ public class ExecutionTime {
         }
     }
 
+    /*
+    * Calculated run time. For the calculation, the start and end times are
+    * encapsulated (also predefined by calling the corresponding setters).
+    */
     public long calculateExecutionTime() {
         return BigDecimal.valueOf(this.startMillis).subtract(
                 BigDecimal.valueOf(this.endMillis), MathContext.DECIMAL64)
