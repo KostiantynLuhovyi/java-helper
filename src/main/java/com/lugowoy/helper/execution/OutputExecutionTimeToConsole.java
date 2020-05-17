4d55c04a6faab0a321d2db90fbeb33b18f44a4c7
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-17 23:36:33 +0300

    feature(execution) Changing, refactoring and docs code in the OutputExecutionTimeToConsole class
    Now the class implements the contract of the interface OutputExecutionTime;
    
    The boolean isOnlyMilliseconds attribute is added that indicates the formatting style of the appearance of the execution time when it is output to the console. Added getter and setter. A detailed description is in the documentation;
    
    Overloaded constructors are created;
    
    In the output(final long milliseconds) method, a check for the positive value of the argument in milliseconds has been added.
    
    Written code documentation.

--- src/main/java/com/lugowoy/helper/execution/OutputExecutionTimeToConsole.java
+++ src/main/java/com/lugowoy/helper/execution/OutputExecutionTimeToConsole.java
@@ -1,16 +1,21 @@
 package com.lugowoy.helper.execution;
 
+import com.lugowoy.helper.checkers.CheckerNumber;
+
 import java.util.concurrent.TimeUnit;
 
 /**
+ * The class provides an implementation of the output of the execution time on
+ * the console, thereby implementing the contract declares in the interface
+ * {@link OutputExecutionTime}.
+ * <p>
  * Created by Konstantin Lugowoy on 06.02.2020.
  *
  * @author Konstantin Lugowoy
- * @version 1.1
+ * @version 1.2
  * @since 2.7
  */
-//todo write doc's
-public final class OutputExecutionTimeToConsole {
+public final class OutputExecutionTimeToConsole implements OutputExecutionTime {
 
     private static final String TIME_HOURS_AND_THEN =
             "\nRuntime: %d hours, %d minutes, %d seconds, %d milliseconds \n";
@@ -21,19 +26,120 @@ public final class OutputExecutionTimeToConsole {
     private static final String TIME_MILLISECONDS =
             "\nRuntime: %d milliseconds \n";
 
-    public static void outputExecutionTime(final long timeInMilliseconds) {
-        long hours = calculateToHours(timeInMilliseconds);
-        long minutes = calculateToMinutes(timeInMilliseconds);
-        long seconds = calculateToSeconds(timeInMilliseconds);
-        long milliseconds = calculateRemainingMilliseconds(timeInMilliseconds,
-                                                           hours, minutes,
-                                                           seconds);
-        showExecutionTime(hours, minutes, seconds, milliseconds);
+    private boolean isOnlyMilliseconds = true;
+
+    /**
+     * Constructs an object of this class.<br>The attribute characterizing the
+     * formatting appearance of the execution time has {@code true} value, mean
+     * the time of the execution will in the format - only milliseconds. This
+     * field can changed by calling the corresponding setter of this attribute.
+     */
+    public OutputExecutionTimeToConsole() {
     }
 
-    private static void showExecutionTime(final long hours, final long minutes,
-                                          final long seconds,
-                                          final long milliseconds) {
+    /**
+     * Constructs an object of this class by initializing its attribute to
+     * select a formatting option for the appearance of the execution time.<br>
+     * The formatting of the appearance of the runtime depends on several
+     * factors. The {@code isOnlyMilliseconds} argument in the object of this
+     * class {@code false}, then the time of the execution outputs only in
+     * milliseconds. If this attribute {@code true}, then the time in
+     * milliseconds calculated in hours, seconds, milliseconds and output.
+     *
+     * @param isOnlyMilliseconds the value that determines of the formatting
+     * appearance of the execution time.
+     */
+    public OutputExecutionTimeToConsole(final boolean isOnlyMilliseconds) {
+        this.isOnlyMilliseconds = isOnlyMilliseconds;
+    }
+
+    /**
+     * Outputs the time {@code milliseconds} of execution.<br>The appearance for
+     * outputting the time value implemented in a class that implements an
+     * interface contract.<br>The output to the console using by calling the
+     * {@link java.io.PrintStream#printf(String, Object...)} method. The
+     * formatting of the appearance of the time of the execution depends on
+     * several factors. The encapsulated field in the object of this class
+     * {@code false}, then the time output only in milliseconds. If this
+     * attribute {@code true}, then the time in milliseconds passed by the
+     * argument {@code milliseconds} calculated in hours, seconds, milliseconds
+     * and outputs.
+     *
+     * @param milliseconds the time to output.
+     *
+     * @throws IllegalArgumentException if the {@code milliseconds} argument
+     * value is negative.
+     */
+    @Override
+    public void output(final long milliseconds) {
+        if (CheckerNumber.isPositive(milliseconds) || CheckerNumber.isZero(
+                milliseconds)) {
+            if (this.isOnlyMilliseconds()) {
+                System.out.printf(TIME_MILLISECONDS, milliseconds);
+            } else {
+                long hours = this.calculateHours(milliseconds);
+                long minutes = this.calculateMinutes(milliseconds);
+                long seconds = this.calculateSeconds(milliseconds);
+                long remainingMillis = this.calculateRemainingMilliseconds(
+                                        milliseconds, hours, minutes, seconds);
+                this.showExecutionTime(hours, minutes, seconds, remainingMillis);
+            }
+        } else {
+            throw new IllegalArgumentException(
+                            "Negative value of the argument in milliseconds");
+        }
+    }
+
+    /**
+     * Gets the value of the attribute that determines the formatting appearance
+     * of the execution time.
+     *
+     * @return the value that determines of the frmatting appearance of the
+     * execution time.
+     */
+    public boolean isOnlyMilliseconds() {
+        return isOnlyMilliseconds;
+    }
+
+    /**
+     * Sets the value of the attribute that determines the appearance of the
+     * execution time. A value of true indicates that the execution time will
+     * output in milliseconds, and a value of false indicates a more detailed
+     * appearance of the execution time.
+     *
+     * @param isOnlyMilliseconds the value that determines of the formatting
+     * appearance of the execution time.
+     */
+    public void setOnlyMilliseconds(final boolean isOnlyMilliseconds) {
+        this.isOnlyMilliseconds = isOnlyMilliseconds;
+    }
+
+    //TODO: Get rid of magic numbers.
+
+    private long calculateHours(final long milliseconds) {
+        return ((milliseconds / (1000 * 60 * 60)) % 24);
+    }
+
+    private long calculateMinutes(final long milliseconds) {
+        return ((milliseconds / (1000 * 60)) % 60);
+    }
+
+    private long calculateSeconds(final long milliseconds) {
+        return (milliseconds / 1000) % 60;
+    }
+
+    private long calculateRemainingMilliseconds(final long milliseconds,
+                                                final long hours,
+                                                final long minutes,
+                                                final long seconds) {
+        return milliseconds - (TimeUnit.HOURS.toMillis(hours)
+                                    + TimeUnit.MINUTES.toMillis(minutes)
+                                        + TimeUnit.SECONDS.toMillis(seconds));
+    }
+
+    private void showExecutionTime(final long hours, final long minutes,
+                                   final long seconds,
+                                   final long milliseconds) {
         if (hours >= 1) {
             System.out.printf(TIME_HOURS_AND_THEN, hours, minutes, seconds,
                               milliseconds);
@@ -45,28 +151,6 @@ public final class OutputExecutionTimeToConsole {
         } else {
             System.out.printf(TIME_MILLISECONDS, milliseconds);
         }
-    }
-
-    //TODO: Get rid of magic numbers
-
-    private static long calculateToHours(final long timeInMilliseconds) {
-        return ((timeInMilliseconds / (1000 * 60 * 60)) % 24);
-    }
-
-    private static long calculateToMinutes(final long timeInMilliseconds) {
-        return ((timeInMilliseconds / (1000 * 60)) % 60);
-    }
-
-    private static long calculateToSeconds(final long timeInMilliseconds) {
-        return (timeInMilliseconds / 1000) % 60;
-    }
-
-    private static long calculateRemainingMilliseconds(long milliseconds,
-                                                       long hours, long minutes,
-                                                       long seconds) {
-        milliseconds -= TimeUnit.HOURS.toMillis(hours) + TimeUnit.MINUTES
-                .toMillis(minutes) + TimeUnit.SECONDS.toMillis(seconds);
-        return milliseconds;
     }
 
 }
