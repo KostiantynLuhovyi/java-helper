04059c85972dcf9e79d9759a17892fd7f94d0af7
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-17 23:29:27 +0300

    feature(execution) Changing, refactoring and docs code in the OutputExecutionTime interface
    Renamed outputExecutionTime(long milliseconds) method to output(long milliseconds);
    
    Written code documentation.

--- src/main/java/com/lugowoy/helper/execution/OutputExecutionTime.java
+++ src/main/java/com/lugowoy/helper/execution/OutputExecutionTime.java
@@ -1,15 +1,22 @@
 package com.lugowoy.helper.execution;
 
 /**
+ * The interface declares a contract to output the time execution any task.
+ * <p>
  * Created by Konstantin Lugowoy on 11.09.2019.
  *
  * @author Konstantin Lugowoy
- * @version 1.8
+ * @version 1.9
  * @since 1.7.6
  */
-//todo write doc's
 public interface OutputExecutionTime {
 
-    void outputExecutionTime(long milliseconds);
+    /**
+     * Outputs the time of the execution. The appearance for outputting the time
+     * value implemented in a class that implements an interface contract.
+     *
+     * @param milliseconds the time of the execution to output.
+     */
+    void output(long milliseconds);
 
 }
