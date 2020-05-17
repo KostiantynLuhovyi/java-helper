dfccac27a65656733a14f00421af4b8440ab5c6f
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-17 23:11:05 +0300

    feature(execution) Changing, refactoring and docs code in the OutputExecutionResult interface
    Constants are removed from the interface (moved to the class OutputExecutionResultToConsole).
    
    The method outputExecutionResult(T t) is renamed to output(T tResult);
    
    Added a new method with signature output(String msgOutputResult, T tResult);
    
    Written code documentation.

--- src/main/java/com/lugowoy/helper/execution/OutputExecutionResult.java
+++ src/main/java/com/lugowoy/helper/execution/OutputExecutionResult.java
@@ -1,19 +1,33 @@
 package com.lugowoy.helper.execution;
 
 /**
+ * The interface declares a contract to output the result execution any task.
+ * <p>
  * Created by Konstantin Lugowoy on 12.09.2019.
  *
  * @author Konstantin Lugowoy
- * @version 1.8
+ * @version 1.9
  * @since 1.7.8
  */
 public interface OutputExecutionResult {
 
-    String RESULT_STRING = "Result : %s";
-    String RESULT_INTEGER = "Result : %d";
-    String RESULT_DOUBLE = "Result : %.3f";
-    String RESULT_BOOLEAN = "Result : %b";
+    /**
+     * Outputs the result of the execution, formalizing its appearance as
+     * a default string.
+     *
+     * @param <T> the type of the result.
+     * @param tResult the result to output.
+     */
+    <T> void output(T tResult);
 
-    <T> void outputExecutionResult(T t);
+    /**
+     * Outputs the result of the execution, formalizing its appearance as
+     * a {@code msgOutputResult} string.
+     *
+     * @param <T> the type of the result.
+     * @param msgOutputResult the string for the appearance of the result.
+     * @param tResult the result to output.
+     */
+    <T> void output(String msgOutputResult, T tResult);
 
 }
