c3eb34886f43234a85fab503d63007da34ce508d
Konstantin Lugowoy <kostya.lugowoy@gmail.com>
2020-05-18 23:21:20 +0300

    docs(execution) Documentation changes in the class ExecutionTime

--- src/main/java/com/lugowoy/helper/execution/ExecutionTime.java
+++ src/main/java/com/lugowoy/helper/execution/ExecutionTime.java
@@ -7,20 +7,19 @@ import java.math.MathContext;
 
 /**
  * The class provides functionality for calculating the execution time of a
- * task.<br> The start time of the task in milliseconds, and the end time are
- * indicated, and the result is the value of the difference in milliseconds.
- * When creating an object of this class, its attributes the start and
- * end times are initialized with the value 0. To indicate the start of the run
- * time, one of the setters must be called to set the start time.
- * To end the run time, you also need to call the corresponding setter.
- * To get the result, namely the difference between the end time
- * and the start time, can be obtained by calling the {@link
- * ExecutionTime#calculateExecutionTime()} method.
+ * task.<p>The start time of the task in milliseconds, and the end time
+ * indicated, and the result the value of the difference in milliseconds. When
+ * creating an object of this class, its attributes the start and end times
+ * initialized with the value {@literal 0L}. To indicate the start of the run
+ * time, one of the setters must called to set the start time. To end the time
+ * of the execution, you also need to call the corresponding setter. Gets the
+ * result, namely the difference between the end time, and the start time by
+ * calling the {@link ExecutionTime#calculateExecutionTime()} method.
  * <p>
  * Created by Konstantin Lugowoy on 13.09.2019.
  *
  * @author Konstantin Lugowoy
- * @version 1.5
+ * @version 1.6
  * @since 1.7.8
  */
 public class ExecutionTime {
@@ -29,16 +28,16 @@ public class ExecutionTime {
     private long endMillis = 0L;
 
     /**
-     * Set the start time in milliseconds.<br> The start value in milliseconds
-     * is taken from the method {@link System#currentTimeMillis()} call.
+     * Set the start time in milliseconds. The start value in milliseconds
+     * taken from the method {@link System#currentTimeMillis()} call.
      */
     public void setStartTime() {
         this.startMillis = System.currentTimeMillis();
     }
 
     /**
-     * Set the start time in milliseconds.<br> The start value in milliseconds
-     * is taken from the {@code milliseconds} argument.
+     * Set the start time in milliseconds. The start value in milliseconds
+     * taken from the {@code milliseconds} argument.
      *
      * @param milliseconds the start time in milliseconds.
      *
@@ -56,16 +55,16 @@ public class ExecutionTime {
     }
 
     /**
-     * Set the end time in milliseconds.<br> The end value in milliseconds is
-     * taken from the method {@link System#currentTimeMillis()} call.
+     * Set the end time in milliseconds. The end value in milliseconds taken
+     * from the method {@link System#currentTimeMillis()} call.
      */
     public void setEndTime() {
         this.endMillis = System.currentTimeMillis();
     }
 
     /**
-     * Set the end time in milliseconds.<br> The end value in milliseconds is
-     * taken from the {@code milliseconds} argument.
+     * Set the end time in milliseconds. The end value in milliseconds taken
+     * from the {@code milliseconds} argument.
      *
      * @param milliseconds the end time in milliseconds.
      *
@@ -82,10 +81,13 @@ public class ExecutionTime {
         }
     }
 
-    /*
-    * Calculated run time. For the calculation, the start and end times are
-    * encapsulated (also predefined by calling the corresponding setters).
-    */
+    /**
+     * Calculates the time of the execution.<br>For the calculation, the start
+     * and end times encapsulated (also predefined by calling the corresponding
+     * setters).
+     *
+     * @return the execution time in the milliseconds.
+     */
     public long calculateExecutionTime() {
         return BigDecimal.valueOf(this.startMillis).subtract(
                 BigDecimal.valueOf(this.endMillis), MathContext.DECIMAL64)
