e50527f5e259a3b8cb18c4dcb42a807d9f1b38dc
Konstantin <kostya.lugowoy@gmail.com>
2020-09-03 23:17:27 +0300

    refactor+docs(models) Refactoring and write docs in the AbstractArray class

--- src/main/java/com/lugowoy/helper/models/arrays/AbstractArray.java
+++ src/main/java/com/lugowoy/helper/models/arrays/AbstractArray.java
@@ -2,18 +2,24 @@ package com.lugowoy.helper.models.arrays;
 
 import com.lugowoy.helper.checkers.CheckerArray;
 import com.lugowoy.helper.models.Model;
+import com.lugowoy.helper.utils.LengthOutOfRangeException;
 
 import java.util.Objects;
 import java.util.RandomAccess;
 
 /**
+ * The abstract class describing the root for the hierarchy of classes
+ * representing dynamic arrays for storing various objects.<br>The class
+ * contains a size attribute that is used to determine the length of a
+ * dynamic array. There are also accessor methods (public getter and
+ * protected setter) to this attribute.
+ * <p>
  * Created by Konstantin Lugowoy on 16.10.2019.
  *
  * @author Konstantin Lugowoy
- * @version 1.4
+ * @version 1.5
  * @since 2.0
  */
-//todo write doc's
 public abstract class AbstractArray implements Model, RandomAccess {
 
     private static final int ARRAY_HEADER = 8;
@@ -38,51 +44,32 @@ public abstract class AbstractArray implements Model, RandomAccess {
     private int size;
     private int modCount = 0;
 
+    /**
+     * Constructs an object of an array (for the heir class), with size(length)
+     * {@link AbstractArray#DEFAULT_SIZE}.
+     */
     AbstractArray() {
         this.size = DEFAULT_SIZE;
     }
 
-    AbstractArray(final int lengthArray) {
-        CheckerArray.checkLength(lengthArray);
-        this.size = LOWER_SIZE;
+    /**
+     * Constructs an object of an array (for the heir class) by initializing it
+     * with the size(length) of the {@code size} argument. The argument {@code
+     * size} must range (inclusive, respectively) from {@link
+     * AbstractArray#LOWER_SIZE} to {@link AbstractArray#UPPER_SIZE}.
+     *
+     * @param size the size(length) of the created array.
+     *
+     * @throws LengthOutOfRangeException if the {@code size} value out of range
+     * ​​from {@link AbstractArray#LOWER_SIZE} to {@link AbstractArray#UPPER_SIZE}.
+     */
+    AbstractArray(final int size) {
+        CheckerArray.checkLength(size, AbstractArray.UPPER_SIZE);
+        this.size = size;
     }
 
     /**
-     * Indicates whether some other object is "equal to" this one.
-     * <p>
-     * The {@code equals} method implements an equivalence relation on non-null
-     * object references:
-     * <ul>
-     *  <li>It is <i>reflexive</i>: for any non-null reference value
-     *  {@code x}, {@code x.equals(x)} should return {@code true}.
-     *  <li>It is <i>symmetric</i>: for any non-null reference values
-     *  {@code x} and {@code y}, {@code x.equals(y)} should return {@code true}
-     *  if and only if {@code y.equals(x)} returns {@code true}.
-     *  <li>It is <i>transitive</i>: for any non-null reference values
-     *  {@code x}, {@code y}, and {@code z}, if {@code x.equals(y)} returns
-     *  {@code true} and {@code y.equals(z)} returns {@code true}, then
-     *  {@code x.equals(z)} should return {@code true}.
-     *  <li>It is <i>consistent</i>: for any non-null reference values
-     *  {@code x} and {@code y}, multiple invocations of {@code x.equals(y)}
-     *  consistently return {@code true} or consistently return {@code false},
-     *  provided no information used in {@code equals} comparisons on the
-     *  objects is modified.
-     *  <li>For any non-null reference value {@code x}, {@code x.equals(null)}
-     *  should return {@code false}.
-     * </ul>
-     * <p>
-     * Note that it is generally necessary to override the {@code hashCode}
-     * method whenever this method is overridden, so as to maintain the
-     * general contract for the {@code hashCode} method, which states
-     * that equal objects must have equal hash codes.
-     *
-     * @param o the reference object with which to compare.
-     *
-     * @return {@code true} if this object is the same as the obj argument;
-     * {@code false} otherwise.
-     *
-     * @see #hashCode()
-     * @see java.util.HashMap
+     * {@inheritDoc}
      */
     @Override
     public boolean equals(final Object o) {
@@ -93,54 +80,36 @@ public abstract class AbstractArray implements Model, RandomAccess {
             return false;
         }
         final AbstractArray that = (AbstractArray) o;
-        return size == that.size;
+        return size == that.size && getModCount() == that.getModCount();
     }
 
     /**
-     * Returns a hash code value for the object. This method is supported for
-     * the benefit of hash tables such as those provided by {@link
-     * java.util.HashMap}.
-     * <p>
-     * The general contract of {@code hashCode} is:
-     * <ul>
-     * <li>Whenever it is invoked on the same object more than once during
-     * an execution of a Java application, the {@code hashCode} method must
-     * consistently return the same integer, provided no information used in
-     * {@code equals} comparisons on the object is modified. This integer need
-     * not remain consistent from one execution of an application to another
-     * execution of the same application.
-     * <li>If two objects are equal according to the {@code equals(Object)}
-     * method, then calling the {@code hashCode} method on each of the two
-     * objects must produce the same integer result.
-     * <li>It is <em>not</em> required that if two objects are unequal
-     * according to the {@link java.lang.Object#equals(java.lang.Object)}
-     * method, then calling the {@code hashCode} method on each of the two
-     * objects must produce distinct integer results.  However, the programmer
-     * should be aware that producing distinct integer results for unequal
-     * objects may improve the performance of hash tables.
-     * </ul>
-     * <p>
-     * As much as is reasonably practical, the hashCode method defined
-     * by class {@code Object} does return distinct integers for
-     * distinct objects. (The hashCode may or may not be implemented
-     * as some function of an object's memory address at some point
-     * in time.)
-     *
-     * @return a hash code value for this object.
-     *
-     * @see Objects#hashCode(Object)
-     * @see java.lang.Object#equals(java.lang.Object)
-     * @see java.lang.System#identityHashCode
+     * {@inheritDoc}
      */
     @Override
     public int hashCode() {
-        return Objects.hashCode(size);
+        return Objects.hash(size, getModCount());
     }
 
+    /**
+     * Gets the size of an array.
+     *
+     * @return the size of an array.
+     */
     public int size() {
         return this.size;
     }
 
+    /**
+     * Sets the size of an array.<br>The argument {@code size} must range
+     * (inclusive, respectively) from {@link AbstractArray#LOWER_SIZE} to {@link
+     * AbstractArray#UPPER_SIZE}.
+     *
+     * @param size the size of an array.
+     *
+     * @throws LengthOutOfRangeException if the {@code size} value out of range
+     * from {@link AbstractArray#LOWER_SIZE} to {@link AbstractArray#UPPER_SIZE}.
+     */
     protected void setSize(final int size) {
         CheckerArray.checkLength(size);
         this.size = size;
