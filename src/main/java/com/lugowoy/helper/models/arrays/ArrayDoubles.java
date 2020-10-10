fa6ee6b5a88023d6063b5320f52fc7e45938e54d
Konstantin <kostya.lugowoy@gmail.com>
2020-10-11 00:00:39 +0300

    refactor(models) Refactoring code in the ArrayDoubles class
    Reorganized imports;
    
    'arrayDoubles' attribute renamed to 'array';
    
    The final modifier is added to the class declaration;
    
    Refactoring constructors. Added various checks to constructor implementations. Final modifiers added to constructor parameters;
    
    Changes in the code of methods overridden from the Object class;
    
    Changes to the implementation of the iterator() method; Reworked anonymous class that implements the Iterable<T> interface;
    
    Method refactoring:
     - forEach(Consumer<Double>);
     - toArray();
     - toArray(double[]);
     - setArray(double);
     - setArray(double...)
     - get(int);
     - set(int, double);
     - add(double);
     - add(int, double);
     - addAll(double...);
     - addAll(int, double...);
     - remove(double);
     - removeByIndex(int);
    
    Added some comments to the execution (//TODO);

--- src/main/java/com/lugowoy/helper/models/arrays/ArrayDoubles.java
+++ src/main/java/com/lugowoy/helper/models/arrays/ArrayDoubles.java
@@ -1,10 +1,13 @@
 package com.lugowoy.helper.models.arrays;
 
-import com.lugowoy.helper.utils.checking.CheckerArray;
-import com.lugowoy.helper.utils.checking.CheckerIndex;
+import com.lugowoy.helper.checkers.CheckerArray;
+import com.lugowoy.helper.checkers.CheckerIndex;
+import org.apache.commons.lang3.SerializationUtils;
 
 import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.function.Consumer;
 
@@ -12,248 +15,289 @@ import java.util.function.Consumer;
  * Created by Konstantin Lugowoy on 16.10.2019.
  *
  * @author Konstantin Lugowoy
- * @version 1.7
+ * @version 1.8
  * @since 2.0
  */
 //todo write doc's
-public class ArrayDoubles extends AbstractArray {
+public final class ArrayDoubles extends AbstractArray {
 
-    private double[] arrayDoubles;
+    private double[] array;
 
     public ArrayDoubles() {
-        this.arrayDoubles = new double[DEFAULT_LENGTH];
+        super();
+        this.array = new double[AbstractArray.DEFAULT_CAPACITY];
     }
 
-    public ArrayDoubles(double[] arrayDoubles) {
-        if (CheckerArray.checkLengthInArray(arrayDoubles)) {
-            this.arrayDoubles = Arrays.copyOf(arrayDoubles, arrayDoubles.length);
-            super.setSize(this.arrayDoubles.length);
-        }
+    public ArrayDoubles(final double... array) {
+        super();
+        CheckerArray.check(array, AbstractArray.UPPER_CAPACITY);
+        this.array = SerializationUtils.clone(array);
+        super.setSize(this.array.length);
     }
 
-    public ArrayDoubles(int lengthArray) {
-        super(lengthArray);
-        this.arrayDoubles = new double[lengthArray];
+    public ArrayDoubles(final int capacity) {
+        super(capacity);
+        this.array = new double[capacity];
     }
 
-    public ArrayDoubles(ArrayDoubles arrayDoubles) {
-        if (CheckerArray.checkLengthInArray(arrayDoubles)) {
-            this.arrayDoubles = Arrays.copyOf(arrayDoubles.toArray(), arrayDoubles.size());
-            super.setSize(this.arrayDoubles.length);
-        }
+    public ArrayDoubles(final ArrayDoubles array) {
+        super();
+        CheckerArray.check(array, AbstractArray.UPPER_CAPACITY);
+        this.array = SerializationUtils.clone(array.array);
+        super.setSize(this.array.length);
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ArrayDoubles)) return false;
-        if (!super.equals(o)) return false;
-        ArrayDoubles that = (ArrayDoubles) o;
-        return Arrays.equals(arrayDoubles, that.arrayDoubles);
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ArrayDoubles)) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        final ArrayDoubles that = (ArrayDoubles) o;
+        return Arrays.equals(array, that.array);
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
-        result = 31 * result + Arrays.hashCode(arrayDoubles);
+        result = 31 * result + Arrays.hashCode(array);
         return result;
     }
 
     @Override
     public String toString() {
-        Iterator<Double> it = iterator();
-        if ( ! it.hasNext()) {
+        if (this.isEmpty()) {
             return "[]";
         }
         StringBuilder sb = new StringBuilder();
         sb.append('[');
-        for (;;) {
-            sb.append(it.next());
-            if ( ! it.hasNext())
-                return sb.append(']').toString();
-            sb.append(',').append(' ');
+        for (int i = 0; i < super.size(); i++) {
+            sb.append(this.get(i)).append(',').append(' ');
         }
+        return sb.append("]").toString();
     }
 
     @Override
-    protected ArrayDoubles clone() throws CloneNotSupportedException {
-        ArrayDoubles arrayDoubles = (ArrayDoubles) super.clone();
-        arrayDoubles.setArray(Arrays.copyOf(this.arrayDoubles, super.size()));
-        return arrayDoubles;
+    public ArrayDoubles clone() {
+        ArrayDoubles cloneArrayDoubles;
+        try {
+            cloneArrayDoubles = (ArrayDoubles) super.clone();
+            cloneArrayDoubles.setArray(SerializationUtils.clone(this.array));
+        } catch (CloneNotSupportedException ex) {
+            throw new UnsupportedOperationException(ex);
+        }
+        return cloneArrayDoubles;
     }
 
     public Iterator<Double> iterator() {
         return new Iterator<>() {
 
-            private int cursorIteratorElement = 0;
+            private int cursorIterator = 0;
+            private int lastReturned = -1;
+            private int expectedModCount = ArrayDoubles.super.getModCount();
 
             @Override
             public boolean hasNext() {
-                return this.cursorIteratorElement != ArrayDoubles.super.size();
+                return this.cursorIterator != ArrayDoubles.super.size();
             }
 
             @Override
             public Double next() {
-                return ArrayDoubles.this.get(this.cursorIteratorElement++);
+                this.checkModification();
+                if (this.cursorIterator >= ArrayDoubles.super.size()) {
+                    throw new NoSuchElementException();
+                }
+                if (this.cursorIterator >= ArrayDoubles.this.array.length) {
+                    throw new ConcurrentModificationException();
+                }
+                this.lastReturned = this.cursorIterator;
+                this.cursorIterator++;
+                return ArrayDoubles.this.array[this.lastReturned];
             }
 
-            /**
-             * Removes from the underlying collection the last element returned
-             * by this iterator (optional operation).  This method can be called
-             * only once per call to {@link #next}.
-             * <p>
-             * The behavior of an iterator is unspecified if the underlying collection
-             * is modified while the iteration is in progress in any way other than by
-             * calling this method, unless an overriding class has specified a
-             * concurrent modification policy.
-             * <p>
-             * The behavior of an iterator is unspecified if this method is called
-             * after a call to the {@link #forEachRemaining forEachRemaining} method.
-             *
-             * @throws UnsupportedOperationException if the {@code remove}
-             *                                       operation is not supported by this iterator
-             * @throws IllegalStateException         if the {@code next} method has not
-             *                                       yet been called, or the {@code remove} method has already
-             *                                       been called after the last call to the {@code next}
-             *                                       method
-             * @implSpec The default implementation throws an instance of
-             * {@link UnsupportedOperationException} and performs no other action.
-             */
             @Override
             public void remove() {
-                ArrayDoubles.this.remove(--this.cursorIteratorElement);
+                if (this.lastReturned < 0) {
+                    throw new IllegalStateException();
+                }
+                this.checkModification();
+                try {
+                    ArrayDoubles.this.remove(this.lastReturned);
+                    this.cursorIterator = this.lastReturned;
+                    this.lastReturned = -1;
+                    this.expectedModCount = ArrayDoubles.super.getModCount();
+                } catch (IndexOutOfBoundsException ex) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+
+            @Override
+            public void forEachRemaining(
+                    final Consumer<? super Double> action) {
+                throw new UnsupportedOperationException("Not realize.");
+            }
+
+            private void checkModification() {
+                if (this.expectedModCount != ArrayDoubles.super.getModCount()) {
+                    throw new ConcurrentModificationException();
+                }
             }
         };
     }
 
-    public void forEach(Consumer<Double> action) {
-        Objects.requireNonNull(action);
-        Double aDouble = null;
-        for (double tmpDouble : this.arrayDoubles) {
-            aDouble = tmpDouble;
+    public void forEach(final Consumer<Double> action) {
+        Objects.requireNonNull(action, "Action is null.");
+        final int expectedModCount = super.getModCount();
+        final int size = super.size();
+        for (int i = 0; i < size; i++) {
+            if (expectedModCount == super.getModCount()) {
+                action.accept(this.get(i));
+            } else {
+                throw new ConcurrentModificationException();
+            }
         }
-        action.accept(aDouble);
     }
 
     public double[] toArray() {
-        return Arrays.copyOf(this.arrayDoubles, super.size());
+        return Arrays.copyOf(this.array, super.size());
     }
 
     public double[] toArray(double[] array) {
-        if (CheckerArray.checkLengthInArray(array)) {
-            array = Arrays.copyOf(this.arrayDoubles, super.size());
+        CheckerArray.check(array, AbstractArray.UPPER_CAPACITY);
+        if (array.length < super.size()) {
+            array = Arrays.copyOf(this.array, super.size());
         }
+        System.arraycopy(this.array, 0, array, 0, super.size());
         return array;
     }
 
-    public void setArray(double[] arrayDoubles) {
-        if (CheckerArray.checkLengthInArray(arrayDoubles)) {
-            this.arrayDoubles = Arrays.copyOf(arrayDoubles, arrayDoubles.length);
-            super.setSize(this.arrayDoubles.length);
-        }
+    public void setArray(final int capacity) {
+        CheckerArray.checkLength(capacity, AbstractArray.UPPER_CAPACITY);
+        this.array = new double[capacity];
+        super.setSize(capacity);
+        super.setModCount(AbstractArray.START_MOD_COUNT);
     }
 
-    public void setArray(int lengthArray) {
-        if (CheckerArray.checkLengthArray(lengthArray)) {
-            this.arrayDoubles = new double[lengthArray];
-            super.setSize(SIZE_ZERO);
-        }
+    public void setArray(final double... array) {
+        CheckerArray.check(array, AbstractArray.UPPER_CAPACITY);
+        this.array = Arrays.copyOf(array, array.length);
+        super.setSize(this.array.length);
+        super.setModCount(AbstractArray.START_MOD_COUNT);
     }
 
     public boolean isEmpty() {
         return super.size() == 0;
     }
 
-    public double get(int index) {
-        CheckerIndex.checkIndex(index, this.size());
-        return this.arrayDoubles[index];
+    public double get(final int index) {
+        CheckerIndex.checkInRange(index, super.size());
+        return this.array[index];
     }
 
-    public double set(int index, double element) {
-        Objects.checkIndex(index, this.size());
+    public double set(final int index, final double element) {
+        CheckerIndex.checkInRange(index, AbstractArray.UPPER_CAPACITY);
         double oldValue = this.get(index);
-        this.arrayDoubles[index] = element;
+        this.array[index] = element;
         return oldValue;
     }
 
-    public boolean add(double element) {
-        if (super.size() == this.arrayDoubles.length) {
-            this.arrayDoubles = Arrays.copyOf(this.arrayDoubles, this.arrayDoubles.length * 2);
+    public boolean add(final double element) {
+        if (super.size() >= this.array.length) {
+            this.ensureCapacity();
         }
-        this.arrayDoubles[super.size()] = element;
-        super.setSize(super.size() + 1);
-        return true;
+        this.array[super.size()] = element;
+        boolean additionResult = false;
+        if (this.isExactlyAdded(element)) {
+            additionResult = true;
+            super.increaseSize();
+            super.increaseModCount();
+        }
+        return additionResult;
     }
 
-    public void add(int index, double element) {
-        super.checkIndexToAddByIndex(index);
-        if (super.size() == this.arrayDoubles.length) {
-            this.arrayDoubles = Arrays.copyOf(this.arrayDoubles, this.arrayDoubles.length * 2);
+    public void add(final int index, final double element) {
+        CheckerIndex.checkInRange(index, super.size() + 1);
+        if (super.size() >= this.array.length) {
+            this.ensureCapacity();
         }
-        System.arraycopy(this.arrayDoubles, index, this.arrayDoubles, index + 1, super.size() - index);
-        this.arrayDoubles[index] = element;
-        super.setSize(super.size() + 1);
+        System.arraycopy(this.array, index, this.array, index + 1,
+                         super.size() - index);
+        this.array[index] = element;
+        super.increaseSize();
+        super.increaseModCount();
     }
 
-    public boolean addAll(double[] arrayDoubles) {
+    public boolean addAll(final double[] array) {
+        CheckerArray.check(array, AbstractArray.UPPER_CAPACITY);
         boolean resultAddAll = false;
-        if (Objects.nonNull(arrayDoubles)) {
-            if (arrayDoubles.length != 0) {
-                if (arrayDoubles.length > (this.arrayDoubles.length - super.size())) {
-                    this.arrayDoubles = Arrays.copyOf(this.arrayDoubles, (this.arrayDoubles.length * 2) + arrayDoubles.length);
-                }
-                System.arraycopy(arrayDoubles, 0, this.arrayDoubles, super.size(), arrayDoubles.length);
-                super.setSize(super.size() + arrayDoubles.length);
-                resultAddAll = true;
+        //TODO refactoring this. delete this 'if'
+        if (array.length > 0) {
+            if (array.length > (this.array.length - super.size())) {
+                this.array = Arrays.copyOf(this.array, (this.array.length << 1)
+                                                       + array.length);
             }
-        } else {
-            throw new NullPointerException("Collection argument is null.");
+            System.arraycopy(array, 0, this.array, this.size(), array.length);
+            super.increaseSize(array.length);
+            super.increaseModCount();
+            resultAddAll = true;
         }
         return resultAddAll;
     }
 
-    public boolean addAll(int index, double[] arrayDoubles) {
-        super.checkIndexToAddByIndex(index);
+    public boolean addAll(final int index, final double[] array) {
+        CheckerIndex.checkInRange(index, super.size() + 1);
+        CheckerArray.check(array, AbstractArray.UPPER_CAPACITY);
         boolean resultAddAll = false;
-        if (Objects.nonNull(arrayDoubles)) {
-            if (arrayDoubles.length != 0) {
-                if (arrayDoubles.length > (this.arrayDoubles.length - super.size())) {
-                    this.arrayDoubles = Arrays.copyOf(this.arrayDoubles, (this.arrayDoubles.length * 2) + arrayDoubles.length);
-                }
-                int numMoved = super.size() - index;
-                if (numMoved > 0) {
-                    System.arraycopy(this.arrayDoubles, index, this.arrayDoubles, index + arrayDoubles.length, numMoved);
-                }
-                System.arraycopy(arrayDoubles, 0, this.arrayDoubles, index, arrayDoubles.length);
-                super.setSize(super.size() + arrayDoubles.length);
-                resultAddAll = true;
+        //TODO refactoring this. delete this 'if'
+        if (array.length > 0) {
+            if (array.length > (this.array.length - super.size())) {
+                this.array = Arrays.copyOf(this.array, (this.array.length << 1)
+                                                       + array.length);
             }
-        } else {
-            throw new NullPointerException("Argument array of double numbers is null");
+            int numMoved = super.size() - index;
+            if (numMoved > 0) {
+                System.arraycopy(this.array, index, this.array,
+                                 index + array.length, numMoved);
+            }
+            System.arraycopy(array, 0, this.array, index, array.length);
+            super.increaseSize(array.length);
+            super.increaseModCount();
+            resultAddAll = true;
         }
         return resultAddAll;
     }
 
-    public boolean remove(double element) {
-        boolean resultRemove = false;
+    //TODO add methods:
+    // addAll(ArrayDoubles array);
+    // addAll(int index, ArrayDoubles array);
+
+    public boolean remove(final double element) {
+        boolean resultOfRemoving = false;
         for (int i = 0; i < super.size(); i++) {
-            if (element == this.arrayDoubles[i]) {
+            if (element == this.get(i)) {
                 this.removeByIndex(i);
-                resultRemove = true;
+                resultOfRemoving = true;
                 break;
             }
         }
-        return resultRemove;
+        return resultOfRemoving;
     }
 
-    public double removeByIndex(int index) {
+    public double removeByIndex(final int index) {
         Objects.checkIndex(index, super.size());
-        double[] newArray = new double[super.size() - 1];
+        double[] newArray = new double[this.array.length - 1];
         double resultRemove = this.get(index);
-        System.arraycopy(this.arrayDoubles, 0, newArray, 0, index);
-        System.arraycopy(this.arrayDoubles, index + 1, newArray, index, super.size() - (index + 1));
-        this.arrayDoubles = newArray;
-        super.setSize(super.size() - 1);
+        System.arraycopy(this.array, 0, newArray, 0, index);
+        System.arraycopy(this.array, index + 1, newArray, index,
+                         super.size() - (index + 1));
+        this.array = newArray;
+        super.decreaseSize();
+        super.increaseModCount();
         return resultRemove;
     }
 
