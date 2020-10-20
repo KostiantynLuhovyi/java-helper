00077d29306e33c745ccf58129c205221f19f34f
Konstantin <kostya.lugowoy@gmail.com>
2020-10-20 15:27:14 +0300

    feature(utils) Adding a Capacity enumeration
    Added Capacity enumeration. Contains constant fields initialized by the lower and upper limit values ​​of the capacity for various objects. Will be used for arrays, matrices and strings, instead of repeating constants throughout the code.

---
+++ src/main/java/com/lugowoy/helper/utils/Capacity.java
@@ -0,0 +1,24 @@
+package com.lugowoy.helper.utils;
+
+/**
+ * Created by Konstantin Lugowoy on 17.10.2020.
+ *
+ * @author Konstantin Lugowoy
+ * @version 1.0
+ * @since 3.0
+ */
+//TODO documentation
+public enum Capacity {
+    LOWER(0), UPPER(2147483639);
+
+    private final int capacity;
+
+    Capacity(final int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int get() {
+        return this.capacity;
+    }
+
+}
