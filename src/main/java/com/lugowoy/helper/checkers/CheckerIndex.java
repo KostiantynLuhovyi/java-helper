f5071cdc61b2588eb39c11262676309170d21c67
Konstantin <kostya.lugowoy@gmail.com>
2021-01-04 22:35:05 +0200

    feature(checkers) Adding new static methods to the CheckerIndex class

--- src/main/java/com/lugowoy/helper/checkers/CheckerIndex.java
+++ src/main/java/com/lugowoy/helper/checkers/CheckerIndex.java
@@ -2,6 +2,7 @@ package com.lugowoy.helper.checkers;
 
 import com.lugowoy.helper.utils.BoundOutOfRangeException;
 import com.lugowoy.helper.utils.BoundsComparisonException;
+import com.lugowoy.helper.utils.IndicesComparisonException;
 
 /**
  * This class contains static functionality for checking the index value for
@@ -10,7 +11,7 @@ import com.lugowoy.helper.utils.BoundsComparisonException;
  * Created by Konstantin Lugowoy on 08.06.2019
  *
  * @author Konstantin Lugowoy
- * @version 1.6
+ * @version 1.7
  * @since 1.6.6
  */
 //TODO review documentation
@@ -114,6 +115,112 @@ public final class CheckerIndex {
             throw new IndexOutOfBoundsException(
                     "Index value " + index + " out of range from "
                     + lowerBoundIndex + " to " + upperBoundIndex);
+        }
+    }
+
+    public static boolean isLowerEqualUpper(final int lowerIndex,
+                                            final int upperIndex) {
+        return CheckerBoundNumber.isLowerEqualUpper(lowerIndex, upperIndex);
+    }
+
+    public static void checkLowerEqualUpper(final int lowerIndex,
+                                            final int upperIndex) {
+        if (!CheckerBoundNumber.isLowerEqualUpper(lowerIndex, upperIndex)) {
+            throw new IndicesComparisonException(
+                    "The lower index is not equal to upper index");
+        }
+    }
+
+    public static boolean isLowerNotEqualUpper(final int lowerIndex,
+                                               final int upperIndex) {
+        return CheckerBoundNumber.isLowerNotEqualUpper(lowerIndex, upperIndex);
+    }
+
+    public static void checkLowerNotEqualUpper(final int lowerIndex,
+                                               final int upperIndex) {
+        if (!CheckerBoundNumber.isLowerNotEqualUpper(lowerIndex, upperIndex)) {
+            throw new IndicesComparisonException(
+                    "The lower index is equal to upper index");
+        }
+    }
+
+    public static boolean isLowerLessUpper(final int lowerIndex,
+                                           final int upperIndex) {
+        return CheckerBoundNumber.isLowerLessUpper(lowerIndex, upperIndex);
+    }
+
+    public static void checkLowerLessUpper(final int lowerIndex,
+                                           final int upperIndex) {
+        if (!CheckerBoundNumber.isLowerLessUpper(lowerIndex, upperIndex)) {
+            throw new IndicesComparisonException(
+                    "The lower index less than upper index");
+        }
+    }
+
+    public static boolean isLowerGreaterUpper(final int lowerIndex,
+                                              final int upperIndex) {
+        return CheckerBoundNumber.isLowerGreaterUpper(lowerIndex, upperIndex);
+    }
+
+    public static void checkLowerGreaterUpper(final int lowerIndex,
+                                              final int upperIndex) {
+        if (!CheckerBoundNumber.isLowerGreaterUpper(lowerIndex, upperIndex)) {
+            throw new IndicesComparisonException(
+                    "The lower index greater than upper index");
+        }
+    }
+
+    public static boolean isLowerLessOrEqualUpper(final int lowerIndex,
+                                                  final int upperIndex) {
+        return CheckerBoundNumber.isLowerLessOrEqualUpper(lowerIndex, upperIndex);
+    }
+
+    public static void checkLowerLessOrEqualUpper(final int lowerIndex,
+                                                  final int upperIndex) {
+        if (!CheckerBoundNumber.isLowerLessOrEqualUpper(lowerIndex, upperIndex)) {
+            throw new IndicesComparisonException(
+                    "The lower index greater or not equal upper bound");
+        }
+    }
+
+    public static boolean isLowerGreaterOrEqualUpper(final int lowerIndex,
+                                                     final int upperIndex) {
+        return CheckerBoundNumber.isLowerGreaterOrEqualUpper(lowerIndex,
+                                                             upperIndex);
+    }
+
+    public static void checkLowerGreaterOrEqualUpper(final int lowerIndex,
+                                                     final int upperIndex) {
+        if (!CheckerBoundNumber.isLowerGreaterOrEqualUpper(lowerIndex, upperIndex)) {
+            throw new IndicesComparisonException(
+                    "The lower index less or not equal upper bound");
+        }
+    }
+
+    public static boolean isLowerLessOrNotEqualUpper(final int lowerIndex,
+                                                     final int upperIndex) {
+        return CheckerBoundNumber.isLowerLessOrNotEqualUpper(lowerIndex, upperIndex);
+    }
+
+    public static void checkLowerLessOrNotEqualUpper(final int lowerIndex,
+                                                     final int upperIndex) {
+        if (!CheckerBoundNumber.isLowerLessOrNotEqualUpper(lowerIndex, upperIndex)) {
+            throw new IndicesComparisonException(
+                    "The lower index greater or equal upper index");
+        }
+    }
+
+    public static boolean isLowerGreaterOrNotEqualUpper(final int lowerIndex,
+                                                        final int upperIndex) {
+        return CheckerBoundNumber.isLowerGreaterOrNotEqualUpper(lowerIndex,
+                                                                upperIndex);
+    }
+
+    public static void checkLowerGreaterOrNotEqualUpper(final int lowerIndex,
+                                                        final int upperIndex) {
+        if (!CheckerBoundNumber.isLowerGreaterOrNotEqualUpper(lowerIndex, upperIndex)) {
+            throw new IndicesComparisonException(
+                    "The lower index less or equal upper index");
         }
     }
 
